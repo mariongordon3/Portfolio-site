b9fbfe0cdf4985dfcb6ca84f04be388a68792807
Marion Gordon <mariongordon3@yahoo.com>
2023-10-23 18:15:42 -0500

    changed design and added goku under profile pic

--- marion-gordon-portfolio/src/components/navbar.jsx
+++ marion-gordon-portfolio/src/components/navbar.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom"
 export default function Navbar(){
     return(
         <nav>
-          <span>home</span>  
-          <span>projects</span>  
-          <span>journey</span>  
+        <Link className="lnk" to='/'>Home</Link>
+        <Link className="lnk" to='/projects'>Projects</Link>
+        <Link className="lnk" to='/journey'>Journey</Link>
         </nav>
     )
 }
--- marion-gordon-portfolio/src/pages/home.jsx
+++ marion-gordon-portfolio/src/pages/home.jsx
@@ -4,13 +4,18 @@ export default function HomePage(){
             <div className="profile">
                 <img src="linkedinPic.jpg" alt="linkedin picture" />
                 <div>
-                    <h3>
-                        goku stands here
-                    </h3>
+                    <div className="goku">
+                        <img src="goku__spirit_bomb__render__sdbh_world_mission__by_maxiuchiha22_dd5by3a-fullview.png" alt="goku_holding_spirit_bomb" />
+                    </div>
                 </div>
             </div>
             <div className="aboutMe" >
-                <h3>about me goes here</h3>
+                <h2>
+                    About Me
+                </h2>
+                <p>
+                I am a military veteran. I served in the U.S. Navy as a hospital corpsman for 8 years where I was responsible for rendering emergency medical treatment to service members. After my time in the military I got my license as a physical therapist assistant and began working at Swedish Covenant hospital where I have been working for the past 4 years. At my current job I am responsible for providing physical therapy treatment sessions, collaborating with other healthcare professionals in the care of patients, and training family members in taking care of patients at home. During this time I also attended and graduated college with a bachelor of science. Towards the end of my journey in college I was introduced to coding by a friend who has been working as a software engineer for a few years. After months of learning javascript on my own I applied to a 15 week full stack web development bootcamp called Code Platoon which I recently completed.
+                </p>
             </div>
         </div>
     )
