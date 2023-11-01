cd001fd4b0652361a9a49f40bb751dd184b503b3
Marion Gordon <mariongordon3@yahoo.com>
2023-10-31 23:25:25 -0500

    added videos to project page

--- marion-gordon-portfolio/src/pages/projects.jsx
+++ marion-gordon-portfolio/src/pages/projects.jsx
@@ -1,9 +1,12 @@
 export default function Projects (){
     return (
         <>
-            <h1>
-                this is the projects page
-            </h1>
+            <div className="projects">
+                <h3>WhatsGood Nutrition App</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Vkf4XQu5TsI?si=6XXIJzOmdyf__k3T" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <h3>Shredder Fitness App</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/_rA4TZnnPxQ?si=63T2YkExqWKNlLLS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
         </>
     )
 }
