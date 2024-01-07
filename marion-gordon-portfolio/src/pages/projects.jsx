57cbb4f28fd02b42092ed3f5fcc6f138252f0d95
Marion Gordon <mariongordon3@yahoo.com>
2024-01-07 17:52:26 -0600

    started journey page

--- marion-gordon-portfolio/src/pages/projects.jsx
+++ marion-gordon-portfolio/src/pages/projects.jsx
@@ -1,7 +1,7 @@
 export default function Projects (){
     return (
         <>
-            <div className="projects">
+            <div className="homePage">
                 <h3>WhatsGood Nutrition App</h3>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Vkf4XQu5TsI?si=6XXIJzOmdyf__k3T" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <h3>Shredder Fitness App</h3>
